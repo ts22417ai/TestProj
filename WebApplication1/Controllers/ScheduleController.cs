69a0fa6d9982642fd28ceccd6d7f4ef46638c41b
testaccount <ym950507@yahoo.com.tw>
2020-11-13 16:33:15 +0800

    11/13 私廚可預約時間

---
+++ WebApplication1/Controllers/ScheduleController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        // GET: Schedule
+        public ActionResult List(int fCid)
+        {
+
+            var table = from t in (new Database1Entities()).t私廚可預訂時間
+                        where t.fCID == fCid
+                        select t;
+                        
+            List<t私廚可預訂時間> list = new List<t私廚可預訂時間>();
+            foreach (t私廚可預訂時間 t in table)
+            {
+                list.Add(t);
+            }
+            return View(list);
+        }
+        
+        public ActionResult Create(int fCid)
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(t私廚可預訂時間 t)
+        {
+
+            return View();
+        }
+
+    }
+}
