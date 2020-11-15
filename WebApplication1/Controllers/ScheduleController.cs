d5bf57bb2354e5ad3b225a84d6a05510ba8fd222
ts22417ai <ts22417ai@gmail.com>
2020-11-16 02:26:33 +0800

    11/16

--- WebApplication1/Controllers/ScheduleController.cs
+++ WebApplication1/Controllers/ScheduleController.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.Controllers
             var table = from t in (new Database1Entities()).t私廚可預訂時間
                         where t.fCID == fCid
                         select t;
-                        
+
             List<t私廚可預訂時間> list = new List<t私廚可預訂時間>();
             foreach (t私廚可預訂時間 t in table)
             {
@@ -23,7 +23,7 @@ namespace WebApplication1.Controllers
             }
             return View(list);
         }
-        
+
         public ActionResult Create(int fCid)
         {
 
@@ -32,9 +32,50 @@ namespace WebApplication1.Controllers
         [HttpPost]
         public ActionResult Create(t私廚可預訂時間 t)
         {
-
-            return View();
+            using (var db = new Database1Entities())
+            {
+                db.t私廚可預訂時間.Add(t);
+                db.SaveChanges();
+            }
+            return RedirectToAction("List", new { fCid = t.fCID });
         }
 
+        public ActionResult Edit(int id)
+        {
+            var db = (new Database1Entities());
+            var time = db.t私廚可預訂時間.FirstOrDefault(t => t.fTID == id);
+            if(time == null)
+            {
+                return RedirectToAction("List");
+            }
+            return View(time);
+        }
+        [HttpPost]
+        public ActionResult Edit(t私廚可預訂時間 modify)
+        {
+            var db = (new Database1Entities());
+            var time = db.t私廚可預訂時間.FirstOrDefault(t => t.fTID == modify.fTID);
+            if (time == null)
+            {
+                time.f日期 = modify.f日期;
+                time.f晚餐 = modify.f晚餐;
+                time.f午餐 = modify.f午餐;
+
+                db.SaveChanges();
+            }
+            return RedirectToAction("List");
+        }
+
+        public ActionResult Delete(int fTid)
+        {
+            var db = (new Database1Entities());
+            var time = db.t私廚可預訂時間.FirstOrDefault(t=> t.fTID == fTid);
+            if(time != null)
+            {
+                db.t私廚可預訂時間.Remove(time);
+                db.SaveChanges();
+            }
+            return RedirectToAction("List",new { fCid=time.fCID});
+        }
     }
 }
---
+++ WebApplication1/Controllers/SearchResultController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class SearchResultController : Controller
+    {
+        // GET: SearchResult
+        public ActionResult List()
+        {
+            //var table = from p in (new Database1Entities()).tProductitem
+            //            select p;
+            //List<Cproductitem> list = new List<Cproductitem>();
+            //foreach (tProductitem p in table)
+            //    list.Add(new Cproductitem(p));
+           return View();
+        }
+    }
+}
